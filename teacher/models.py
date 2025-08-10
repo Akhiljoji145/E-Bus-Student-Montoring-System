6ffa512bba1ec41069105f042cd851d3d37169fa
Akhiljoji145 <akhiljoji1451@gmail.com>
2025-08-10 20:26:46 +0530

    created models for store teachers details

--- teacher/models.py
+++ teacher/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 class report(models.Model):
-    stud_name=models.CharField(max_length=50)
+    stud_id=models.CharField(max_length=50)
     bus_id=models.IntegerField()
     description=models.TextField()
     time=models.DateField(auto_now=True)
+    teacher_id=models.IntegerField()
+class teacher(models.Model):
+    teacher_name=models.CharField(max_length=50)
+    email=models.models.EmailField(max_length=254)
+    password=models.CharField(max_length=50,blank=True)
+    class_no=models.IntegerField()
+    branch=models.CharField(max_length=50)
+    sem=models.CharField(max_length=50)
 
-    
