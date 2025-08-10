46ef98c8b89427a723f6fbe6657277cfa9f0d534
Akhiljoji145 <akhiljoji1451@gmail.com>
2025-08-10 14:36:04 +0530

    created a report models

--- teacher/models.py
+++ teacher/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 # Create your models here.
 class report(models.Model):
-    
+    stud_name=models.CharField(max_length=50)
+    bus_id=models.IntegerField()
+    description=models.TextField()
+    time=models.DateField(auto_now=True)
+
     
